c6ca1ac1a66dcd1b33fe9874bbe634145fae8bec
tugi <blueqwinn@gmail.com>
2023-03-30 00:23:28 +0300

    add db.json

--- my-blog-app/src/App.js
+++ my-blog-app/src/App.js
@@ -1,16 +1,29 @@
 import Navbar from './Navbar';
-import Home from './home';
-import BlogList from './BlogList'
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
-    <div className="App">
-   <Navbar />
-    <div className="content">
-     <Home />
-     <BlogList />
-     </div>
-  </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
