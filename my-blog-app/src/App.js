f66c194dbcf4e44b76ed4f109cfb8c065c217402
tugi <blueqwinn@gmail.com>
2023-03-30 00:09:06 +0300

    add fetch

--- my-blog-app/src/App.js
+++ my-blog-app/src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Home from './home';
+import BlogList from './BlogList'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
    <Navbar />
     <div className="content">
      <Home />
+     <BlogList />
      </div>
   </div>
   );
--- my-blog-app/src/BlogList.js
+++ my-blog-app/src/BlogList.js
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import BlogList from "./BlogList";
-
-const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  return (
-    <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-    </div>
-  );
-}
- 
-export default Home;
+const BlogList = ({ blogs }) => {
+    return (
+      <div className="blog-list">
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+   
+  export default BlogList;
