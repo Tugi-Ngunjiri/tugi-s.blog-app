cd1f1edcc639337f953d293b16b1010eb2b9bcbc
tugi <blueqwinn@gmail.com>
2023-03-29 23:23:26 +0300

    add part 11

--- my-blog-app/src/App.js
+++ my-blog-app/src/App.js
@@ -1,24 +1,14 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  //const person = { name: 'yoshi', age:30 };
-
   return (
+    <div className="App">
+   <Navbar />
     <div className="content">
-      <h1> {title}</h1>
-      <p>Liked: {likes} times</p>
-      {/*{person} */}
-
-      <p>{10}</p>
-      <p>{"hello, people"}</p>
-      <p>{ [1,2,3,4,5]}</p>
-    <p>{ Math.random() * 18}</p>
-
-    <a href={link}>Google Site</a>
-
-    </div>
+     <Home />
+     </div>
+  </div>
   );
 }
 
